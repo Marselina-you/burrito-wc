8278f3b44e23faec99c48411dea7ca11d44d36a2
Marselina-you <marselina88@list.ru>
2023-09-30 06:44:54 +0300

    video 7.2
    кнопка autoicncrment(woocommerce quantity autoincrement), атрибуты товара

--- wp-content/themes/burrito/footer.php
+++ wp-content/themes/burrito/footer.php
@@ -29,31 +29,5 @@ do_action( 'bur_footer_parts' ); ?>
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
-<script>
-		const buttons = document.querySelectorAll('.single_add_to_cart_button');
-const adds = document.querySelectorAll('.quantity');
-const plus = document.querySelectorAll('.plus');
-const minus = document.querySelectorAll('.minus');
-const inputs = document.querySelectorAll('.input-text');
 
-console.log(buttons);
-console.log(adds);
-console.log(plus);
-console.log(minus);
-console.log(inputs);
-
-buttons.forEach(el => {
-
-    el.addEventListener('click', (e) => {
-        let btn = e.currentTarget;
-    console.log(btn)
-    let ad = btn.nextElementSibling;
-    console.log(ad)
-
-    }); 
-}
-)
-
-
-</script>
 </body></html>
